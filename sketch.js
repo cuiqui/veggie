034038f0a13fea7b4137d26b97a1299890893e16
Juan Schandin <jschandin@gmail.com>
2024-03-12 13:41:14 -0300

    Use quadtree to speed up simulation

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
-let SIM_DIM, PANEL_DIM, CANVAS_DIM;
+let SIM_DIM
+let PANEL_DIM
+let CANVAS_DIM;
 const EPSILON = 1e-6;
+
+const lerp = (a, b, t) => a * (1 - t) + b * t;
 
 class SpeciesData {
     constructor({name, growth, oldage, shade, color, maxsize, offspring}) {
@@ -47,8 +51,8 @@ class Tree {
     }
 
     intersects(other) {
-        let dst = p5.Vector.dist(this.pos, other.pos);
-        let linemgn = this.radius + other.radius;
+        let dst = p5.Vector.sub(this.pos, other.pos).magSq();
+        let linemgn = (this.radius + other.radius) * (this.radius + other.radius);
 
         if (dst - linemgn < EPSILON) {
             return true;
@@ -57,7 +61,8 @@ class Tree {
     }
 
     getRandomPointAroundSpawn() {
-        let randomPoint = () => p5.Vector.add(this.pos, p5.Vector.mult(p5.Vector.random2D(), this.radius * 2));
+        let dst = lerp(this.radius * 3, this.radius * 4, random(1));
+        let randomPoint = () => p5.Vector.add(this.pos, p5.Vector.mult(p5.Vector.random2D(), dst));
         let point = randomPoint();
         while (!isWithinCanvasBounds(point.x, point.y)) {
             point = randomPoint();
@@ -68,17 +73,27 @@ class Tree {
     get oldage() {
         return this.radius >= species[this.species].maxsize;
     }
+
+    get x() {
+        return this.pos.x;
+    }
+
+    get y() {
+        return this.pos.y;
+    }
 }
 
 class Simulator {
-    static TREE_COUNT = 100;
+    static TREE_COUNT = 50;
 
-    constructor() {
+    constructor(qt) {
+        this.qt = qt;
         this.trees = new Set();
         this.reset();
     }
 
     reset() {
+        this.qt.clear()
         this.createTrees();
     }
 
@@ -87,6 +102,7 @@ class Simulator {
             this.resolveDominance(tree);
         }
 
+        let newQt = new Quadtree(this.qt.bounds);
         let newTrees = new Set();
         for (let t of this.trees) {
             let shaded = random(1) < species[t.species].shade;
@@ -95,40 +111,43 @@ class Simulator {
 
             if (t.dominated && shaded) {
                 // Production 1
-                ;
+                newQt.insert(t);
             } else if (t.dominated) {
                 // Produciton 2
                 this.trees.delete(t);
             } else if (t.oldage && survive) {
                 // Production 3
-                ;
+                newQt.insert(t);
             } else if (t.oldage) {
                 // Production 4
                 this.trees.delete(t);
             } else {
                 // Production 5: grow and spawn
                 t.grow();
-
+                newQt.insert(t);
                 if (offspring) {
-                    newTrees.add(new Tree({
+                    let tree = new Tree({
                         species: t.species,
                         pos: t.getRandomPointAroundSpawn()
-                    }));
+                    })
+                    newTrees.add(tree);
+                    newQt.insert(tree);
                 }
             }
         }
-
+        this.qt = newQt;
         this.trees = this.trees.union(newTrees);
-        console.log(`Trees: ${this.trees.size}`);
     }
 
     createTrees() {
         this.trees = new Set();
         for (let i = 0; i < Simulator.TREE_COUNT; i++) {
-            this.trees.add(new Tree({
+            let tree = new Tree({
                 species: random(Object.keys(species)),
                 pos: createVector(random(SIM_DIM.x), random(SIM_DIM.y))
-            }));
+            })
+            this.trees.add(tree);
+            this.qt.insert(tree);
         }
     }
 
@@ -139,7 +158,7 @@ class Simulator {
     }
 
     resolveDominance(t1) {
-        for (const t2 of this.trees) {
+        for (const t2 of this.qt.retrieve(t1)) {
             if (t1 != t2 && t1.intersects(t2)) {
                 if (t1.radius < t2.radius) {
                     t1.dominated = true;
@@ -155,15 +174,15 @@ function createSpecies() {
     species = {
         elm: new SpeciesData({
             name: 'elm', growth: 0.2, oldage: 0.96, shade: 0.7,
-            color: color('green'), maxsize: 15, offspring: 0.2
+            color: color('green'), maxsize: 7, offspring: 0.4
         }),
         palm: new SpeciesData({
-            name: 'palm', growth: 1, oldage: 0.8, shade: 0.4,
-            color: color('orange'), maxsize: 10, offspring: 0.6
+            name: 'palm', growth: 0.6, oldage: 0.8, shade: 0.4,
+            color: color('orange'), maxsize: 7, offspring: 0.5
         }),
         bush: new SpeciesData({
-            name: 'bush', growth: 1, oldage: 0.4, shade: 0.8,
-            color: color('red'), maxsize: 5, offspring: 0.98
+            name: 'bush', growth: 0.75, oldage: 0.4, shade: 0.8,
+            color: color('red'), maxsize: 3, offspring: 0.8
         })
     };
 }
@@ -172,20 +191,24 @@ function isWithinCanvasBounds(x, y) {
     return x >= 0 && x <= SIM_DIM.x && y >= 0 && y <= SIM_DIM.y;
 }
 
-const timeBetweenSteps = 0.1;  // seconds
+const timeBetweenSteps = 0.01;  // seconds
 let species = {};
 let simulator;
 let timeSinceLastStep = 0;  // seconds
 let playing = false;
 
 function setup() {
-    SIM_DIM = createVector(500, 500);
+    SIM_DIM = createVector(800, 800);
     PANEL_DIM = createVector(200, 0);
     CANVAS_DIM = p5.Vector.add(SIM_DIM, PANEL_DIM)
 
     createCanvas(CANVAS_DIM.x, CANVAS_DIM.y);
     createSpecies();
-    simulator = new Simulator();
+
+    let qt = new Quadtree({
+        x: 0, y: 0, width: SIM_DIM.x, height: SIM_DIM.y
+    });
+    simulator = new Simulator(qt);
 
     // Step button
     button = createButton("Step");
